873c25ac7c6a952bbb98834bcd526678f1e46ee4
anthonp <anthony.a.picciano@gmail.com>
2023-06-21 21:26:50 -0600

    Add files via upload

--- net-juicer-slim.sh
+++ net-juicer-slim.sh
@@ -2,10 +2,14 @@
 # 6/21/2023 Anthony Picciano - Network Troubleshooting
 # Depends: python3, ip, curl, netstat
 # Vars
-IP_CIDR="$(ip -o -f inet addr show | awk '/scope global/ {print $4}')"
+IP_CIDR="$(ip -o -f inet addr show | awk '/scope global/ {print $4}' | tr '\n' ' ')"
 ETH="$(ip route list | grep default | cut -d " " -f5)"
 ROUTE="$(ip route list | grep default | cut -d " " --fields=2,3,7)"
 PING="$(ping 8.8.8.8 -c 3)"
+DUPLEXSTATE="$(cat /sys/class/net/${ETH}/duplex)"
+MAC="$(cat /sys/class/net/${ETH}/address)"
+STATE="$(cat /sys/class/net/${ETH}/operstate)"
+LANSPEED="$(cat /sys/class/net/${ETH}/speed)"
 RED='\e[0;31m'
 BLUE='\e[0;34m'
 GREEN='\e[0;32m'
@@ -27,7 +31,8 @@ echo " -------------------------------------------------------------------------
 echo -e " ${WHITE}Hostname:"
 echo -e "   ∟ ${BLUE}$(hostname)${WHITE}"
 echo " ${ETH}:";
-echo -e "   ∟ ${BLUE}${IP_CIDR}${WHITE} ${ROUTE}"
+echo -e "   ∟ ${BLUE}${IP_CIDR}${WHITE} (${MAC}) ${ROUTE}"
+echo -e "   ∟ Status: Link ${RED}${STATE}${WHITE} LAN/Link Speed: ${RED}${LANSPEED} Mbps${WHITE} Duplex State: ${RED}${DUPLEXSTATE}${WHITE} "
 # ICMP Ping (8.8.8.8)
 echo " ----------------------------------------"
 echo "${PING}"
